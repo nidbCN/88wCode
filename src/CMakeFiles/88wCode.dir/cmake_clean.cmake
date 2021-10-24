82fb3070656d831f0aa15dd0050caec608f74615
Gaein nidb <mail@gaein.cn>
2021-10-24 10:29:49 +0800

    Add cmake file

---
+++ src/CMakeFiles/88wCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "88wCode"
+  "88wCode.pdb"
+  "CMakeFiles/88wCode.dir/code.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/88wCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/88wCode.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 88wCode.
+# This may be replaced when dependencies are built.
